c8cc5a46d76289a88ddc0bde0587de9838e1b383
wilcoxch <wilcoxch@sonoma.edu>
2017-08-30 10:35:02 -0700

    added comments

--- TEST3/FileParser.java
+++ TEST3/FileParser.java
@@ -9,6 +9,7 @@ import java.lang.*;
 
 public class FileParser{
 
+    // Class to hold a basic accident record and all relevant parsed information.
     private class AccidentRecord{
 	public int tableNum;
 	public int subTableNum;
@@ -35,6 +36,7 @@ public class FileParser{
 
     }
 
+    // Class to hold a parsed record specific to a year.
     private class YearRecord{
 	public int year;
 	public int totalAccidents;
@@ -53,6 +55,7 @@ public class FileParser{
 	}
     }
 
+    // Class to hold a parsed record specific to a day.
     private class DayRecord{
 	public String day;
 	public int totalAccidents;
@@ -67,6 +70,7 @@ public class FileParser{
 	}
     }
 
+    // class to compare two Accident Records based on year.
     public class yearComparator implements Comparator<AccidentRecord>{
 
 	public int compare(AccidentRecord a, AccidentRecord b){
@@ -74,6 +78,7 @@ public class FileParser{
 	}
     }
 
+    // class to compare two Accident Records based on day.
     public class dayComparator implements Comparator<AccidentRecord>{
 
 	public int compare(AccidentRecord a, AccidentRecord b){
@@ -82,22 +87,29 @@ public class FileParser{
     }
 
 
+    // ArrayLists to hold the compiled records, Y & W are neccessary to keep sorted lists separate as the weekday list relies on the year list for computing daily deaths.
     private ArrayList<AccidentRecord> compiledRecords = new ArrayList<AccidentRecord>();
     private ArrayList<AccidentRecord> compiledRecordsY = new ArrayList<AccidentRecord>();
     private ArrayList<AccidentRecord> compiledRecordsW = new ArrayList<AccidentRecord>();
+
+    // Arraylists to hold the final compiled Yearly and Weekday records.
     private ArrayList<YearRecord> compiledYearlyRecords = new ArrayList<YearRecord>();
     private ArrayList<DayRecord> compiledWeekdayRecords = new ArrayList<DayRecord>();
 
+
+    // Constructor that parses a given file name, then passes each line of that file to be parsed, sorted, and grouped.
     public FileParser(String fileName){
 
 	try {
 	    FileReader fileReader = new FileReader(fileName);
 	    BufferedReader bufferedReader = new BufferedReader(fileReader);
 	    while (bufferedReader.ready()) {
+		// Read each line and parse it into a record.
 		String record = bufferedReader.readLine();
 		parseRecord(record);
 	    }
 	    bufferedReader.close();
+	    //Sort and group each record.
 	    groupByYear();
 	    System.out.println();
 	    groupByWeekday();
@@ -110,6 +122,8 @@ public class FileParser{
 	}
     }
 
+
+    // Parses a record line by line, and stores all relevant info into each compiled record list.
     private void parseRecord(String record){
 	String[] sp = record.trim().split("\\s+");
 	//        for (int i = 0; i < splited.length; i++){
@@ -133,7 +147,7 @@ public class FileParser{
     }
 
 
-
+    // Function to enumerate the weekday of an accident record.
     public int enumerateWeekday(AccidentRecord a){
 	if (a.day.equals("Sun"))
 	    return 0;
@@ -152,8 +166,11 @@ public class FileParser{
 
 
 
+    // Groups accident records by year and then compiles them into a considated year record.
     public void groupByYear(){
+	// Sort on year.
 	Collections.sort(compiledRecordsY, new yearComparator());
+	// Set up variables to count aggregates.
 	int yearCount = compiledRecordsY.get(0).year;
 	int totalDistinctAccidents = 0;
 	double averageWound = 0.0;
@@ -161,6 +178,8 @@ public class FileParser{
 	double averageKill = 0.0;
 	int totalKill = 0;
 
+
+	// For each record, compile aggregates while the year stays the same. Once the year is incremented, create a year record and continue.
 	for (int i = 0; i < compiledRecordsY.size(); i++){
 	    while (yearCount == compiledRecordsY.get(i).year){
 		totalDistinctAccidents++;
@@ -185,7 +204,9 @@ public class FileParser{
 
     }
 
+    // Groups accident records by weekday and then compiles them into a consolidated weekday record.
     public void groupByWeekday(){
+	// Sort and set up initial aggregate variables
 	Collections.sort(compiledRecordsW, new dayComparator());
 	int totalDistinctAccidents = 0;
 	int totalWound = 0;
@@ -193,6 +214,7 @@ public class FileParser{
 	int index = 0;
 	String d = "Sun";
 
+	// For each day of the week, compile aggregates and store them.
 	for (int i = 0; i < 7; i++){
 	    while(index < compiledRecordsW.size() && enumerateWeekday(compiledRecordsW.get(index)) == i){
 		totalDistinctAccidents++;
@@ -211,30 +233,40 @@ public class FileParser{
 
     }
 
+
+    // Since there is no record of daily deaths, deaths must be calculated by finding the specific
+    // record in the Compiled yearly records, then subtracting all aggregate deaths before that day in the same year.
     public int getDeathOfDay(AccidentRecord a){
 	int x = 0;
+	// Find the record we are searching for in the yearly records
 	while (a.recordNum != compiledRecordsY.get(x).recordNum){
 	    x++;
 	}
+
+	// if first record, return that total.
 	if (x == 0){
 	    return a.runningDeathTotal;
 	}
+
+	// if record is found, check to see if there are dates before the given record, and subtract the running death total of the given day from the previous day.
 	else if (a.recordNum == compiledRecordsY.get(x).recordNum){
 	    if (compiledRecordsY.get(x).year == compiledRecordsY.get(x-1).year)
 		return compiledRecordsY.get(x).runningDeathTotal - compiledRecordsY.get(x-1).runningDeathTotal;
+	    // otherwise return the deaths for that given day.
 	    else return compiledRecordsY.get(x).runningDeathTotal;
 	}
-
 
 	else return 0;
     }
 
+    // Function to print the basic Accident Records
     public void printRecords(){
 	for (int i = 0; i < compiledRecords.size(); i++){
 	    System.out.println(compiledRecords.get(i).year + " --- " + compiledRecords.get(i).day);
 	}
     }
 
+    // Function to print Yearly Records
     public void printYearlyRecords(){
 	System.out.println("BY YEAR");
 	for (int i = 0; i < compiledYearlyRecords.size(); i++){
@@ -248,6 +280,7 @@ public class FileParser{
 	System.out.println("-------------------");
     }
 
+    // Function to print Weekday Records
     public void printWeekdayRecords(){
 	System.out.println("BY WEEKDAY");
 	for (int i = 0; i < compiledWeekdayRecords.size(); i++){
@@ -259,6 +292,7 @@ public class FileParser{
 	System.out.println("-------------------");
     }
 
+    // Main Function to take a command line argument file name and parse it.
     public static void main(String[] args){
 	if (args.length == 1){
 	    FileParser f = new FileParser(args[0]);
